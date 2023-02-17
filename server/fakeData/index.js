513b67983610ecd4e28148fe0705cd6b986c6bc1
Minh Tri <mitidevus@gmail.com>
2023-02-17 13:01:39 +0700

    Cấu hình GraphQL, liên kết các type trong Schema & Resolver

---
+++ server/fakeData/index.js
@@ -0,0 +1,32 @@
+export default {
+  authors: [
+    {
+      id: 123,
+      name: "Minh Tri",
+    },
+    {
+      id: 999,
+      name: "Chi Hien",
+    },
+  ],
+  folders: [
+    {
+      id: "1",
+      name: "Home",
+      createdAt: "2022-11-18T03:42:13Z",
+      authorId: 123,
+    },
+    {
+      id: "2",
+      name: "New Folder",
+      createdAt: "2022-10-18T03:42:13Z",
+      authorId: 999,
+    },
+    {
+      id: "3",
+      name: "Work",
+      createdAt: "2022-09-18T03:42:13Z",
+      authorId: 123,
+    },
+  ],
+};
