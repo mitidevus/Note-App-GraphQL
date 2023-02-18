d1e8bb34f13cd90ba1691af38a04aa89b384657f
Minh Tri <mitidevus@gmail.com>
2023-02-18 21:02:24 +0700

    Xây dựng layout cho Home Page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import "../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -12,9 +13,9 @@ import { Container } from "@mui/system";
 import "./firebase/config";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Container maxWidth="lg" sx={{ textAlign: "center", marginTop: "50px" }}>
-      <RouterProvider router={router} />
-    </Container>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Container maxWidth="lg" sx={{ textAlign: "center", marginTop: "50px" }}>
+            <RouterProvider router={router} />
+        </Container>
+    </React.StrictMode>
 );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,33 @@
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import { Outlet } from "react-router-dom";
+import FolderList from "../components/FolderList";
+import UserMenu from "../components/UserMenu";
 
 export default function Home() {
-  return <div>Home</div>;
+    return (
+        <>
+            <Typography variant="h4" sx={{ mb: "20px" }}>
+                Note App
+            </Typography>
+            <Box sx={{ display: "flex", justifyContent: "right", mb: "10px" }}>
+                <UserMenu />
+            </Box>
+
+            <Grid container sx={{ height: "50vh", boxShadow: "0 0 15px 0 rgb(193 193 193 / 60%)" }}>
+                <Grid item xs={3} style={{ height: "100%" }}>
+                    <FolderList
+                        folders={[
+                            { id: "1", name: "Plan for Tet holiday" },
+                            { id: "2", name: "Homework" },
+                        ]}
+                    ></FolderList>
+                </Grid>
+                <Grid item xs={9} style={{ height: "100%" }}>
+                    <Outlet />
+                </Grid>
+            </Grid>
+        </>
+    );
 }
