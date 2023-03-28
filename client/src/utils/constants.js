a5edeccafee81cc2a49dffd91810dee17e6fc4a4
Minh Tri <mitidevus@gmail.com>
2023-03-28 21:47:43 +0700

    Update constants

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,4 @@
+// export const GRAPHQL_SERVER = "http://localhost:4000";
+// export const GRAPHQL_SUBSCRIPTION_ENDPOINT = "ws://localhost:4000";
 export const GRAPHQL_SERVER = "https://note-app-graphql-server.onrender.com";
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = "wss://https://note-app-graphql-server.onrender.com";
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT = "ws://note-app-graphql-server.onrender.com";
